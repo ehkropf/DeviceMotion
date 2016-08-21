0ffb94be127999ee569457e64f573780effe34de
Everett Kropf <ehkropf@gmail.com>
2016-08-21 18:24:54 +0100

    Add velocity integral.

--- DeviceMotion/ViewController.swift
+++ DeviceMotion/ViewController.swift
@@ -12,9 +12,13 @@ import CoreMotion
 class ViewController: UIViewController {
 
     let motionControl = MotionControl()
+    
     var timer: NSTimer?
     var updateInterval: Double = 30 // Hertz
     var isRunning = false
+    
+    var phoneIntegral: VelocityIntegral!
+    var frozenIntegral: VelocityIntegral!
     
     //MARK: View outlets
     
@@ -38,6 +42,10 @@ class ViewController: UIViewController {
         labelX?.textColor = UIColor(CGColor: GVHelpers.graphXColor)
         labelY?.textColor = UIColor(CGColor: GVHelpers.graphYColor)
         labelZ?.textColor = UIColor(CGColor: GVHelpers.graphZColor)
+        
+        let dt = 1.0/updateInterval
+        phoneIntegral = VelocityIntegral(dt: dt)
+        frozenIntegral = VelocityIntegral(dt: dt)
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -58,6 +66,9 @@ class ViewController: UIViewController {
             startStopButton?.setTitle("Start", forState: UIControlState.Normal)
         } else { // not running
             isRunning = true
+            phoneIntegral.reset()
+            frozenIntegral.reset()
+            updateVelocityLabels()
             motionControl.startDeviceMotion()
             timer = NSTimer(timeInterval: NSTimeInterval(1.0/updateInterval), target: self, selector: #selector(timerHandler(_:)), userInfo: nil, repeats: true)
             NSRunLoop.currentRunLoop().addTimer(timer!, forMode: NSDefaultRunLoopMode)
@@ -72,10 +83,17 @@ class ViewController: UIViewController {
         let acc = dm.userAcceleration
         let aref = dm.userAccelerationInReferenceFrame
         
-        graphFrozen?.add(acc.x, acc.y, acc.z)
-        graphPhone?.add(aref.x, aref.y, aref.z)
+        graphPhone?.add(acc.x, acc.y, acc.z)
+        phoneIntegral.add(acceleration: acc)
+        graphFrozen?.add(aref.x, aref.y, aref.z)
+        frozenIntegral.add(acceleration: aref)
+        updateVelocityLabels()
     }
 
+    func updateVelocityLabels() {
+        labelPhoneVelocity?.text = phoneIntegral.string
+        labelFrozenVelocity?.text = frozenIntegral.string
+    }
 }
 
 extension CMAcceleration {
@@ -86,18 +104,55 @@ extension CMAcceleration {
 
 class VelocityIntegral {
     
-    var value: Double = 0
+    var integralValue: [Double] = [0, 0, 0]
+    var value: Double {
+        let v = integralValue
+        return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])
+    }
     var dt: Double
     
     init(dt: Double) {
         self.dt = dt
     }
     
+    func reset() {
+        integralValue = [0, 0, 0]
+    }
+    
     func add(acceleration acc: CMAcceleration) {
-        value += acc.modulus*dt
+        integralValue[0] += acc.x*dt
+        integralValue[1] += acc.y*dt
+        integralValue[2] += acc.z*dt
     }
     
 }
+
+class VelocityFormatter {
+    
+    var _formatter = NSNumberFormatter()
+    
+    init() {
+        _formatter.numberStyle = .DecimalStyle
+        _formatter.minimumIntegerDigits = 1
+        _formatter.minimumFractionDigits = 2
+        _formatter.maximumFractionDigits = 2
+    }
+    
+    func toString(integral: VelocityIntegral) -> String? {
+        return _formatter.stringFromNumber(NSNumber(double: integral.value))
+    }
+    
+}
+
+extension VelocityIntegral {
+    var string: String? {
+        return VelocityFormatter().toString(self)
+    }
+}
+
+
+
+
 
 
 
