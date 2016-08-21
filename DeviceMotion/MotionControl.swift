27ce32d55a1fc6f2240983213ce527d0cf42ea15
Everett Kropf <ehkropf@gmail.com>
2016-08-21 18:26:09 +0100

    Multiply by rotation matrix transpose.

--- DeviceMotion/MotionControl.swift
+++ DeviceMotion/MotionControl.swift
@@ -21,9 +21,9 @@ extension CMDeviceMotion {
         let rot = self.attitude.rotationMatrix
         
         var accRef = CMAcceleration()
-        accRef.x = acc.x*rot.m11 + acc.y*rot.m12 + acc.z*rot.m13
-        accRef.y = acc.x*rot.m21 + acc.y*rot.m22 + acc.z*rot.m23
-        accRef.z = acc.x*rot.m31 + acc.y*rot.m32 + acc.z*rot.m33
+        accRef.x = acc.x*rot.m11 + acc.y*rot.m21 + acc.z*rot.m31
+        accRef.y = acc.x*rot.m12 + acc.y*rot.m22 + acc.z*rot.m23
+        accRef.z = acc.x*rot.m13 + acc.y*rot.m23 + acc.z*rot.m33
         
         return accRef
     }
