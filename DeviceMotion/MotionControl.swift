142612d822368ca4a75d7c8312b5f6836548e335
Everett Kropf <ehkropf@gmail.com>
2016-08-27 12:48:59 +0100

    Rewrite velocity integral.

--- DeviceMotion/MotionControl.swift
+++ DeviceMotion/MotionControl.swift
@@ -55,27 +55,74 @@ class MotionControl {
     }
 }
 
+/**
+ Define a 3D vector.
+ */
+struct Vector3 {
+    var x: Double = 0
+    var y: Double = 0
+    var z: Double = 0
+}
+
+prefix func -(v: Vector3) -> Vector3 {
+    return Vector3(x: -v.x, y: -v.y, z: -v.z)
+}
+
+func +(v1: Vector3, v2: Vector3) -> Vector3 {
+    return Vector3(x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z)
+}
+
+func -(v1: Vector3, v2: Vector3) -> Vector3 {
+    return v1 + -v2
+}
+
+func *(s: Double, v: Vector3) -> Vector3 {
+    return Vector3(x: s*v.x, y: s*v.y, z: s*v.z)
+}
+
+func *(v: Vector3, s: Double) -> Vector3 {
+    return s*v
+}
+
+func /(v: Vector3, s: Double) -> Vector3 {
+    return (1/s)*v
+}
+
+extension Vector3 {
+    init(_ acc: CMAcceleration) {
+        x = acc.x
+        y = acc.y
+        z = acc.z
+    }
+    
+    var magnitude: Double {
+        return sqrt(x*x + y*y + z*z)
+    }
+}
+
+/**
+ Compute the 3D velocity integral bit by bit.
+ */
 class VelocityIntegral {
     
-    var integralValue: [Double] = [0, 0, 0]
-    var value: Double {
-        let v = integralValue
-        return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])
-    }
-    var dt: Double
+    var value = Vector3()
+    private var _lastAdded: Vector3
     
-    init(dt: Double) {
-        self.dt = dt
+    init(v0: Vector3) {
+        _lastAdded = v0
     }
     
     func reset() {
-        integralValue = [0, 0, 0]
+        value = Vector3()
     }
     
-    func add(acceleration acc: CMAcceleration) {
-        integralValue[0] += acc.x*dt
-        integralValue[1] += acc.y*dt
-        integralValue[2] += acc.z*dt
+    func add(acceleration acc: CMAcceleration, dt: Double) {
+        let va = Vector3(acc)
+        let mid = (va + _lastAdded)/2.0
+        _lastAdded = va
+        value.x += mid.x*dt
+        value.y += mid.y*dt
+        value.z += mid.z*dt
     }
     
 }
@@ -92,7 +139,7 @@ class VelocityFormatter {
     }
     
     func toString(integral: VelocityIntegral) -> String? {
-        return _formatter.stringFromNumber(NSNumber(double: integral.value))
+        return _formatter.stringFromNumber(NSNumber(double: integral.value.magnitude))
     }
     
 }
